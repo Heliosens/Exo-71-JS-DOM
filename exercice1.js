01138d79137f5189b27a6708139e2e2165307ea8
Sylvie-B <unsolo@hotmail.fr>
2021-11-17 21:59:10 +0100

    create element remove element

--- exercice1.js
+++ exercice1.js
@@ -1,11 +1,18 @@
-let monDiv = .createElement('div');
+let monDiv = document.createElement('div');
+
+monDiv.innerHTML = "Ce div a été ajouté en utilisant javascript";
 
 document.getElementById('viewport').appendChild(monDiv);
 
+let viewP = document.getElementById('viewport');
 
+let div2 = document.createElement('div');
+div2.style.backgroundColor = "red";
+div2.style.width = "100px";
+div2.style.height = "100px";
 
+viewP.appendChild(div2);
 
+let clear = document.getElementById('aSupprimer');
 
-
-
-
+clear.remove();
